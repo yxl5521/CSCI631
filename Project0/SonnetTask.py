716af2e4d00a7d19e0116b9ef9b30a6874dde526
yxl5521 <yxl5521@rit.edu>
2021-02-23 21:03:24 +0800

    Q3 task 2

--- Project0/SonnetTask.py
+++ Project0/SonnetTask.py
@@ -9,21 +9,31 @@ import cv2
 import skimage.io as io
 
 
-def plot_histogram():
+def plot_histogram(image):
     """
     Plot the histogram of sonnet image
     Use skimage to read in RGB image
 
     :return: None
     """
-    img = io.imread('./images/sonnet.png')
-    plt.hist(img.flatten(), bins=256, range=[0, 256])
+    plt.hist(image.flatten(), bins=256, range=[0, 256])
     plt.ylim(top=3000)
     plt.show()
 
 
+def modify_image(img, threshold):
+    img[img > threshold] = 255
+    img[img <= threshold] = 0
+    window = 'sonnet'
+    cv2.imshow(window, img)
+    cv2.waitKey(0)
+    cv2.destroyWindow(window)
+
+
 def main():
-    plot_histogram()
+    img = io.imread('./images/sonnet.png')
+    plot_histogram(img)
+    modify_image(img, 150)
 
 
 if __name__ == '__main__':
