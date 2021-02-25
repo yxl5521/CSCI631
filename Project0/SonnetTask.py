7288162b7b97e80046c3638fe1f6174825b34068
yxl5521 <yxl5521@rit.edu>
2021-02-25 19:19:12 +0800

    add comment

--- Project0/SonnetTask.py
+++ Project0/SonnetTask.py
@@ -24,6 +24,11 @@ def plot_histogram(image):
 
 
 def normalization(image):
+    """
+    Contrast stretching the image
+    :param image: The image to be normalized
+    :return: The normalized image
+    """
     image = image.copy()
     max = np.amax(image)
     min = np.amin(image)
@@ -36,6 +41,12 @@ def normalization(image):
 
 
 def make_window(block_size):
+    """
+    Make the window for calculating thresholds
+    Will leave the middle(the one need to be replaced) empty
+    :param block_size: the size of the Block
+    :return: The window
+    """
     kernel = np.ones((block_size, block_size))
     mid = int((block_size - 1) / 2)
     kernel[mid, mid] = 0
@@ -43,6 +54,13 @@ def make_window(block_size):
 
 
 def mean_threshold(image, block_size, constant):
+    """
+    Calculate the threshold using mean(N*N) + C formula
+    :param image: The image to convert
+    :param block_size: The size of the block
+    :param constant: The constant to add
+    :return: The calculated thresholds for the image
+    """
     img = image.copy()
     window = make_window(block_size)
     result = ndimage.generic_filter(img, np.nanmean, footprint=window, mode='constant', cval=np.NaN)
@@ -51,6 +69,13 @@ def mean_threshold(image, block_size, constant):
 
 
 def median_threshold(image, block_size, constant):
+    """
+    Calculate the threshold using median(N*N) + C formula
+    :param image: The image to convert
+    :param block_size: The size of the block
+    :param constant: The constant to add
+    :return: The calculated thresholds for the image
+    """
     window = make_window(block_size)
     result = ndimage.generic_filter(image, np.nanmedian, footprint=window, mode='constant', cval=np.NaN)
     threshold = result + constant
@@ -61,12 +86,9 @@ def otsus_method(data, possible_thresholds):
     """
     Use Otsu's Method to get the best threshold
     that can minimize the weighted variance for clusters
-    :param alpha: the alpha for regularization, if None then no regularization needed
+    :param data: all original images
     :param possible_thresholds: all thresholds that can be test
-            the quantized data points
-    :param data is all data in the data set,
-           either all ages or all heights in this assignment
-    :return: the best threshold we found and the minimum mixed variance found
+    :return: the best threshold we found
     """
     # set the initial value to infinite
     # to make the condition always tru at the first time
@@ -101,6 +123,12 @@ def otsus_method(data, possible_thresholds):
 
 
 def find_threshold(image):
+    """
+    Try to find the best threshold
+    Assign a range of block sizes and constants to try
+    :param image: The image to convert
+    :return: The best threshold with the used formula and block size and constant
+    """
     blocks = list(range(3, 20, 2))
     constants = list(range(5, 7))
     thresholds = {}
@@ -115,6 +143,14 @@ def find_threshold(image):
 
 
 def modify_image(img, threshold, result=None):
+    """
+    Make all val larger than the threshold to 255(white)
+    Make all val smaller and equals to the threshold to 0(black)
+    :param img: The image to convert
+    :param threshold:The threshold to compare
+    :param result: The block size, constant and formula used to get this threshold
+    :return: None
+    """
     img = img.copy()
     img[img > threshold] = 255
     img[img <= threshold] = 0
@@ -127,11 +163,16 @@ def modify_image(img, threshold, result=None):
 
 
 def main():
+    # read the image
     img = io.imread('./images/sonnet.png')
+    # plot original histogram
     plot_histogram(img)
+    # use manual threshold to modify the image
     modify_image(img, 106)
+    # do the testing
     normalized = normalization(img)
     best = find_threshold(normalized)
+    # found that median need to be invert to get the best result
     if best[2] == 'median':
         threshold = median_threshold(normalized, best[0], -best[1])
         modify_image(normalized, threshold, (best[0], -best[1], 'median'))
